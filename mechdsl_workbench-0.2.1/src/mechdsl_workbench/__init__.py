afa9919392c09c02d95d61566a8e8344d001e07c
Shmuel Osovski <osovski.technion@gmail.com>
2026-08-31 15:17:00 +0300

    chore: remove stray build byproducts

--- mechdsl_workbench-0.2.1/src/mechdsl_workbench/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Standalone browser workbench for MechDSL and algo2code."""
-
-from __future__ import annotations
-
-__version__ = "0.2.0"
--- mechdsl_workbench-0.2.1/src/mechdsl_workbench/examples/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Bundled workbench examples."""
-
-from .catalog import get_example, list_examples
-
-__all__ = ["get_example", "list_examples"]
